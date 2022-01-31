083b6d7c58e68b9492b1077c87a7ff07e44bde87
Roma <65kgm9ca@gmail.com>
2022-01-31 20:41:47 +0300

    similar posts by tags

--- django_by_example/blog/views.py
+++ django_by_example/blog/views.py
@@ -6,6 +6,7 @@ from django.core.mail import send_mail
 from .forms import EmailPostForm, CommentForm
 from .models import Post, Comment
 from taggit.models import Tag
+from django.db.models import Count
 
 
 # Create your views here.
@@ -69,11 +70,18 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids) \
+        .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')) \
+        .order_by('-same_tags', '-publish')[:4]
+
     context = {
         'post': post,
         'comments': comments,
         'new_comment': new_comment,
-        'comment_form': comment_form
+        'comment_form': comment_form,
+        'similar_posts': similar_posts
     }
 
     return render(request, 'blog/post/detail.html', context)
